c522263b7d0039713e8b1582216bb4405a234dc4
marving <gb694467942@126.com>
2017-07-25 08:42:00 +0800

    整理

---
+++ src/main/java/com/marving/code/java/util/SimpleDateFormatDemo.java
@@ -0,0 +1,23 @@
+package com.marving.code.java.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by mercop on 2017/7/24.
+ */
+
+public class SimpleDateFormatDemo {
+
+    public static void main(String[] args) throws ParseException {
+
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+        String dateString = simpleDateFormat.format(date);
+        System.out.println(dateString);
+
+        Date date1 = simpleDateFormat.parse(dateString);
+        System.out.println(date1);
+    }
+}
